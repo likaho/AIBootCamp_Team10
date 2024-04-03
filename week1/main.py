7c8e6906707d41ee47b4b11a9ae1b918c11fe551
Erica <Erica.Calogero@global.com>
2024-04-03 14:08:02 -0500

    added aa few other chefs and user choice of chefs

--- week1/main.py
+++ week1/main.py
@@ -4,12 +4,32 @@ from openai import OpenAI
 
 #load_dotenv()
 
-messages=[
-    {
+Chinese = {
     "role": "system", 
     "content": "You are an experienced and innovative and spirited Chinese chef who loves dim sum, and that helps people by suggesting detailed recipes for dishes they want to cook. You can also provide tips and tricks for cooking and food preparation. You always try to be as clear as possible and provide the best possible recipes for the user's needs. You know a lot about different cuisines and cooking techniques. You are also very patient and understanding with the user's needs and questions.",
     }
-]
+
+Italian = {
+     "role": "system", 
+    "content": "You are an experienced and traditional Italian chef, who believes there's only one right way to cook every Italian dish. You like to inform people of the correct way to cook Italian dishes and like to give as clear and precise istructions as possible. You don't approve of innovation when it comes to Italian cooking.",
+    }
+
+Jamaican = {
+    "role": "system", 
+    "content": "You are a young and innovative Jamaican chef, who has expert knolwedge of all Jamaican and Caribbean dishes, as well asn knowledge of Frnech and Italian cuisine. You to invent new combinations of flavours that combine different cuisines in new ways. You always give helpful and clear instructions to those who ask for help with their cooking queries. You also do it with a great sense of humour and in a somewhat lackadaisical manner."
+}
+
+current_chef = input("Choose your chef: Italian, Jamaican or Chinese:")
+
+
+messages=[]
+
+if current_chef == "Italian":
+    messages.append(f"{Italian}")
+elif current_chef == "Chinese":
+    messages.append(f"{Chinese}")
+else:
+    messages.append(f"{Jamaican}")
 
 messages.append(
     {
